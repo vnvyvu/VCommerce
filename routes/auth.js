0e3af0445f21f33d698c4cb785d25fc1458aed2f
VyVu <vutrivi99@yahoo.com.vn>
2022-01-09 14:25:40 +0700

    1.0.1: Implement refreshToken

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,22 @@
-const express = require("express");
-const router = express.Router({mergeParams: true});
+const express = require('express');
+const router = express.Router({ mergeParams: true });
 
-const controller = require("../controller/auth");
-const {registerRules, registerValidate} = require("../controller/validator/authValidator");
+const controller = require('../controller/auth');
+const {
+	registerRules,
+	registerValidate,
+	loginRules,
+	loginValidate,
+} = require('../controller/validator/authValidator');
 
-router.post("/register", ...registerRules, registerValidate, controller.register);
-router.post("/login", controller.login);
-router.use("/logout", controller.logout);
+router.post(
+	'/register',
+	...registerRules,
+	registerValidate,
+	controller.register
+);
+router.post('/login', ...loginRules, loginValidate, controller.login);
+router.use('/logout', controller.logout);
+router.post('/token', controller.token);
 
 module.exports = router;
